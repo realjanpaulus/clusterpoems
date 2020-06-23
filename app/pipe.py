b4669a63900606bea374745af7049eee969e12d1
Jan Paulus <janpaulus95@web.de>
2020-06-23 09:47:22 +0200

    refactor: repo structure

---
+++ app/pipe.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+"""TODO
+- mehr corpora
+- mehr Epocheneinteilungen
+- dimensionality reduction
+- KernelPCA zur Visualisierung nutzen? 
+	- siehe GERON, dimension reduction, 226f.
+	- da dann acuh GridSearch machen (GERON, 228)
+- Durchgehen: https://scikit-learn.org/stable/modules/clustering.html#clustering-performance-evaluation
+	- vllt noch weitere, interessante Evaluationsmetriken
+	- nochmal klar machen, was welche metrik verwendet
+"""
+import argparse
+from collections import Counter, defaultdict
+from datetime import datetime
+import json
+import logging
+import numpy as np
+import pandas as pd
+
+
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import adjusted_mutual_info_score, adjusted_rand_score, completeness_score, homogeneity_score
+from sklearn.mixture import GaussianMixture
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder
+
+import sys
+import time
+from utils import add_epoch_division, text_cleaning
+
+
+def main():
+
+	# ================
+	# time managment #
+	# ================
+
+	program_st = time.time()
+
+	# =======================
+	# predefined parameters #
+	# =======================
+
+	n_jobs = args.n_jobs
+	vectorizer = TfidfVectorizer(lowercase=args.lowercase,
+								 max_features=args.max_features,
+								 stop_words=None)
+	ars_dict = {}
+
+	
+	# ================================
+	# classification logging handler #
+	# ================================
+	logging_filename = f"../logs/pipe_{args.corpus_name}.log"
+	logging.basicConfig(level=logging.DEBUG, filename=logging_filename, filemode="w")
+	console = logging.StreamHandler()
+	console.setLevel(logging.INFO)
+	formatter = logging.Formatter("%(levelname)s: %(message)s")
+	console.setFormatter(formatter)
+	logging.getLogger('').addHandler(console)
+
+	
+	# =================
+	# corpus loading  # 
+	# =================
+
+	if args.corpus_name == "poems":
+		corpus = text_cleaning(pd.read_csv("../corpora/german_poems.csv"))
+		text_name = "poem"
+		logging.info(f"Read '{args.corpus_name}' corpus.")
+	else:
+		logging.warning(f"Couldn't find a corpus with the name '{args.corpus_name}'.")
+	
+
+	with open("epochs.json") as f:
+		epochs = json.loads(f.read())
+
+	if args.epoch_division == "brenner":
+		epochs = epochs["brenner"]
+		epoch_exception = "Klassik_Romantik"
+		corpus = add_epoch_division(corpus, epochs, epoch_exception=epoch_exception)
+		logging.info(f"Added epoch division by '{args.epoch_division}'.")
+	else:
+		logging.warning(f"Couldn't find a epoch division with the name '{args.epoch_division}'.")
+
+
+	epoch1 = args.epoch_one
+	epoch2 = args.epoch_two
+	corpus = corpus[(corpus.epoch == epoch1) | (corpus.epoch == epoch2)]
+	logging.info(f"Used epochs are '{epoch1}' and '{epoch2}'.")
+
+
+	text = corpus[text_name].values
+	labels = LabelEncoder().fit_transform(corpus["epoch"].values)
+	unique_epochs = list(np.unique(corpus["epoch"]))
+
+
+	if args.reduce_dimensionality:
+		#TODO
+		nothing = 0
+		...
+	vector = vectorizer.fit_transform(text)
+
+	kmeans_st = time.time()
+	kmeans = KMeans(n_clusters=len(unique_epochs),
+					n_jobs=n_jobs)
+	kmeans.fit(vector)
+
+	print("--------------- Metrics (K-Means) ---------------")
+	kmeans_ars = adjusted_rand_score(labels, kmeans.labels_)
+	logging.info(f"Adjusted Rand Score for K-Means: {kmeans_ars}.")
+
+	kmeans_amis = adjusted_mutual_info_score(labels, kmeans.labels_)
+	logging.info(f"Adjusted Mutuial Info Score for K-Means: {kmeans_amis}.")
+
+	kmeans_hs = homogeneity_score(labels, kmeans.labels_)
+	logging.info(f"Homogeneity Score for K-Means: {kmeans_hs}.")
+
+	kmeans_cs = completeness_score(labels, kmeans.labels_)
+	logging.info(f"Completeness Score for K-Means: {kmeans_cs}.")
+	print("--------------------------------------------------")
+
+	kmeans_duration = float(time.time() - kmeans_st)
+	logging.info(f"Run-time K-Means: {kmeans_duration} seconds")
+
+	"""TODO
+	dbscan_st = time.time()
+	dbscan = DBSCAN(n_jobs=n_jobs)
+	dbscan.fit(vector)
+	dbscan_ars = adjusted_rand_score(labels, dbscan.labels_)
+	logging.info(f"Adjusted Rand Score for DBSCAN: {dbscan_ars}.")
+	dbscan_duration = float(time.time() - dbscan_st)
+	logging.info(f"Run-time DBSCAN: {dbscan_duration} seconds")
+	"""
+
+	"""TODO
+	gmm_st = time.time()
+	gmm = GaussianMixture()
+	gmm.fit(vector)
+	gmm_ars = adjusted_rand_score(labels, gmm.labels_)
+	logging.info(f"Adjusted Rand Score for Gaussian Mixture Model: {gmm_ars}.")
+	gmm_duration = float(time.time() - gmm_st)
+	logging.info(f"Run-time Gaussian Mixture Model: {gmm_duration} seconds")
+	"""
+	
+
+	logging.info(f"Read {args.corpus_name} corpus ({int((time.time() - program_st)/60)} minute(s)).")
+
+
+	# ============
+	# clustering #
+	# ============
+
+	
+	program_duration = float(time.time() - program_st)
+	logging.info(f"Run-time: {int(program_duration)/60} minute(s).")
+
+
+
+if __name__ == "__main__":
+	
+	parser = argparse.ArgumentParser(prog="pipe", description="Pipeline for clustering.")
+	parser.add_argument("--corpus_name", "-cn", type=str, default="poems", help="Indicates the corpus. Default is 'poems'.")
+	parser.add_argument("--epoch_division", "-ed", type=str, default="brenner", help="Indicates the epoch division method.")
+	parser.add_argument("--epoch_one", "-eo", type=str, default="Aufklärung", help="Name of the first epoch.")
+	parser.add_argument("--epoch_two", "-et", type=str, default="Realismus", help="Name of the first epoch.")
+	parser.add_argument("--lowercase", "-l", type=bool, default=False, help="Indicates if words should be lowercased.")
+	parser.add_argument("--max_features", "-mf", type=int, default=60000, help="Indicates the number of most frequent words.")
+	parser.add_argument("--n_jobs", "-nj", type=int, default=1, help="Indicates the number of processors used for computation.")
+	parser.add_argument("--reduce_dimensionality", "-rd", type=bool, default=False, help="Indicates if dimension reduction should be applied before clustering.")
+	parser.add_argument("--save_date", "-sd", action="store_true", help="Indicates if the creation date of the results should be saved.")
+	
+	args = parser.parse_args()
+
+	main()
---
+++ app/utils.py
@@ -0,0 +1,203 @@
+from html import unescape
+from IPython.core.display import HTML
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import re
+
+import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn import metrics
+import spacy
+from stop_words import get_stop_words
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+
+# ================== #
+# corpus processing  #
+# ================== # 
+
+def add_epoch_division(corpus, epochs, epoch_exception=""):
+    """ Divide poems in DataFrame into epochs by dictionary.
+    """
+    df = corpus.copy()
+    epochs_d = {}
+    
+    for epoch, v in epochs.items():
+        if epoch != epoch_exception:
+            epochs_d[epoch] = list(range(epochs[epoch]["b"], epochs[epoch]["e"] + 1))
+    
+    df["epoch"] = df.apply(lambda row: get_epoch(row.year, epochs_d), axis=1)
+    return df
+
+def text_cleaning(corpus):
+    df = corpus.copy()
+    df["poem"] = df.poem.apply(unescape)
+
+    def remove_b(s):
+        regex = re.compile("b'(.*?)'")
+        return re.sub(regex, r"\1", s)
+
+    df["poem"] = df["poem"].apply(remove_b)
+
+    orthography_correction = {"ſ": "s", "uͤ": "ue", "aͤ": "ae", "oͤ": "oe"}
+
+    for k, v in orthography_correction.items():
+        df["poem"] = df["poem"].str.replace(k, v)
+
+    return df
+
+def random_downsampling(corpus, 
+                        class_col = "epoch", 
+                        max_value = 1000):
+    """ Reduces all instances of all classes to a certain maximum value.
+    """   
+
+    classes = list(corpus.epoch.unique())
+    
+    if len(classes) >= 2:
+        corpus_1 = corpus[corpus[class_col] == classes[0]]
+        corpus_2 = corpus[corpus[class_col] == classes[1]]
+        corpus_1 = corpus_1.sample(max_value)
+        corpus_2 = corpus_2.sample(max_value)
+        return pd.concat([corpus_1, corpus_2], axis=0)
+
+    elif len(classes) == 3:
+
+        corpus_1 = corpus[corpus[class_col] == classes[0]]
+        corpus_2 = corpus[corpus[class_col] == classes[1]]
+        corpus_3 = corpus[corpus[class_col] == classes[2]]
+        corpus_1 = corpus_1.sample(max_value)
+        corpus_2 = corpus_2.sample(max_value)
+        corpus_3 = corpus_3.sample(max_value)
+
+        return pd.concat([corpus_1, corpus_2, corpus_3], axis=0)
+
+    elif len(classes) == 4:
+
+        corpus_1 = corpus[corpus[class_col] == classes[0]]
+        corpus_2 = corpus[corpus[class_col] == classes[1]]
+        corpus_3 = corpus[corpus[class_col] == classes[2]]
+        corpus_4 = corpus[corpus[class_col] == classes[3]]
+        corpus_1 = corpus_1.sample(max_value)
+        corpus_2 = corpus_2.sample(max_value)
+        corpus_3 = corpus_3.sample(max_value)
+        corpus_4 = corpus_4.sample(max_value)
+
+        return pd.concat([corpus_1, corpus_2, corpus_3, corpus_4], axis=0)
+
+    else:
+        print(f"Class count of {len(classes)} is too high, no downsampling was performed.")
+        return corpus
+
+
+# ==========
+# clustering
+# ==========
+
+def generate_wordcloud(top_words, pos_remove=False, img_name = ""):
+    plt.rcParams['figure.dpi']= 300
+
+    # Create and generate a word cloud image:
+    wordcloud = WordCloud(width=600,
+                          height=300,
+                          background_color="white").generate_from_frequencies(top_words)
+
+    # Display the generated image:
+    plt.figure(figsize=(8,8))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    #plt.tight_layout(pad=0)
+    plt.axis("off")
+    
+    if pos_remove:
+        plt.savefig(f"img/results/wc{img_name}_top_n_words_pos_remove")
+    else:
+        plt.savefig(f"img/results/wc{img_name}_top_n_words")
+    plt.show()
+
+def get_epoch(year, epochs):
+    epoch = ""
+    for k, v in epochs.items():
+        if year in v:
+            epoch = k
+            break
+    return epoch
+
+def get_json_dict(filename):
+   with open(filename) as f_in:
+       return(json.load(f_in))
+
+def purity_score(y_true, y_pred):
+    # compute contingency matrix (also called confusion matrix)
+    contingency_matrix = metrics.cluster.contingency_matrix(y_true, y_pred)
+    return np.sum(np.amax(contingency_matrix, axis=0)) / np.sum(contingency_matrix)
+
+
+def get_top_n_words(column, n = None, stopwords = "", pos_remove = False):
+    """ Get the top n words of a text column.
+    """
+    texts = list(column)
+    if pos_remove:
+        texts = remove_pos(texts)
+    
+    vectorizer = TfidfVectorizer(max_features=None, stop_words=stopwords)
+    vector = vectorizer.fit_transform(texts)
+    sum_words = vector.sum(axis=0) 
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vectorizer.vocabulary_.items()]
+    words_freq = sorted(words_freq, key = lambda x: x[1], reverse=True)
+    return words_freq[:n]
+
+def remove_pos(lst, used_pos = ["VERB", "ADJ", "NOUN"]):
+    """ Remove every part of speach except the specified exceptions.
+    """
+    nlp = spacy.load('de')
+    new_lst = []
+    for s in lst:
+        posstr = nlp(s)
+        new_string = ""
+        for token in posstr:  
+            if token.pos_ in used_pos:
+                new_string = new_string + token.text + " "
+        new_lst.append(new_string)
+        
+    return new_lst
+    
+
+def wordcloud_epochs(df, epochs, n = 100, pos_remove = False):
+    
+    top_words_dict = {}
+    
+    for k, v in epochs.items():
+        print(k)
+        corpus = df[(df.year >= v[0]) & (df.year <= v[1])]
+        stopwords = get_stop_words("de")
+        top_words = dict(get_top_n_words(corpus["poem"], n=n, stopwords=stopwords, pos_remove = pos_remove))
+        top_words_dict[k] = top_words
+        generate_wordcloud(top_words, pos_remove, img_name = k)
+        
+    return top_words_dict
+
+
+# =====
+# others
+# =====
+
+
+def hide_code():
+    """Hide jupyter notebook code cells."""
+    toggle_code_str = """
+    <form action="javascript:code_toggle()"><input type="submit" id="toggleButton" value="Show/Hide Code Cell"></form>
+    """
+
+    toggle_code_prepare_str = """
+    <script>
+    function code_toggle() {
+        if ($('div.cell.code_cell.rendered.selected div.input').css('display')!='none'){
+            $('div.cell.code_cell.rendered.selected div.input').hide();
+        } else {
+            $('div.cell.code_cell.rendered.selected div.input').show();
+        }
+    }
+    </script>"""
+    display(HTML(toggle_code_prepare_str + toggle_code_str))
