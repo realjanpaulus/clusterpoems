fa622294edeee43955c02529a47c29439f588406
Jan Paulus <janpaulus95@web.de>
2020-07-13 21:49:23 +0200

    edit: pipe

--- app/pipe.py
+++ app/pipe.py
@@ -539,7 +539,7 @@ if __name__ == "__main__":
 	parser.add_argument("--corpus_name", "-cn", type=str, default="poems", help="Indicates the corpus. Default is 'poems'. Another possible value is 'noise'.")
 	parser.add_argument("--epoch_division", "-ed", type=str, default="amann", help="Indicates the epoch division method. Possible values are 'amann', brenner'.")
 	#parser.add_argument("--epoch_exception", "-ee", type=str, default="Klassik_Romantik", help="Indicates the epoch which should be skipped.")
-	parser.add_argument("--epoch_one", "-eo", type=str, default="Barock", help="Name of the first epoch.")
+	parser.add_argument("--epoch_one", "-eo", type=str, default="Naturalismus", help="Name of the first epoch.")
 	parser.add_argument("--epoch_two", "-et", type=str, default="Realismus", help="Name of the first epoch.")
 	parser.add_argument("--lowercase", "-l", type=bool, default=True, help="Indicates if words should be lowercased.")
 	parser.add_argument("--max_features", "-mf", type=int, default=10000, help="Indicates the number of most frequent words.")
